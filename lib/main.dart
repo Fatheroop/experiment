f9d2321f7a120c2185d03ddbb91cca75ad294dbf
Fatheroop <yk5046013@gmail.com>
2026-02-08 12:56:52 +0530

    new

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:code_forge/code_forge.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final CodeForgeController _controller = CodeForgeController();
+  final UndoRedoController _undoRedoController = UndoRedoController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: CodeForge(
+          filePath: "D:\\Flutter projects\\experiment\\lib\\main.dart",
+          controller: _controller,
+          undoController: _undoRedoController,
+        ),
+      ),
+    );
+  }
+}
