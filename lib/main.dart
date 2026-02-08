dcb3403bdc18f28b56fcd50d886e4768ccb4dc80
Fatheroop <yk5046013@gmail.com>
2026-02-08 14:04:35 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,29 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   final CodeForgeController _controller = CodeForgeController();
   final UndoRedoController _undoRedoController = UndoRedoController();
+  bool _ready = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _ready = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: CodeForge(
-          filePath: "D:\\Flutter projects\\experiment\\lib\\main.dart",
-          controller: _controller,
-          undoController: _undoRedoController,
-        ),
+        body: _ready
+            ? CodeForge(
+                filePath: "D:\\Flutter projects\\experiment\\lib\\main.dart",
+                controller: _controller,
+                undoController: _undoRedoController,
+              )
+            : const Center(child: SizedBox()),
       ),
     );
   }
